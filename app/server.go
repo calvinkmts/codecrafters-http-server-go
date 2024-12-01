79b87047efd7d20a15a6e983ed45872bd4a4b51f
Calvin Kamtoso <calvinkmts@gmail.com>
2024-12-01 19:57:21 +0700

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
@@ -15,28 +16,29 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	conn, err = l.Accept()
+
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	// for {
-	// 	conn, err := l.Accept()
-	// 	if err != nil {
-	// 		fmt.Println("Error accepting connection: ", err.Error())
-	// 		os.Exit(1)
-	// 	}
-		
-	// 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	// }
+	buffer := make([]byte, 2048)
+	conn.Read(buffer)
+
+	fmt.Println(string(buffer))
+
+	req := strings.Split(string(buffer), " ")
+
+	if len(req[1]) > 1 {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
